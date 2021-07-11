9b3b4ee0b981a242caf87c6c9ae14d812435e453
Ipsky <yap@usc.edu>
2021-07-11 15:19:31 -0700

    Initial Commit

---
+++ PencilMarkRedux/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PencilMarkRedux
+//
+//  Created by Secret Asian Man Dev on 11/7/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PencilMarkRedux/PencilMarkReduxApp.swift
@@ -0,0 +1,17 @@
+//
+//  PencilMarkReduxApp.swift
+//  PencilMarkRedux
+//
+//  Created by Secret Asian Man Dev on 11/7/21.
+//
+
+import SwiftUI
+
+@main
+struct PencilMarkReduxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
