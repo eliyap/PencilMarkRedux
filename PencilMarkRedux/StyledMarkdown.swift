847f550dbd11df8f4e51eab35d17e7f4169f05a2
Ipsky <yap@usc.edu>
2021-07-12 13:02:35 -0700

    break off conformance

--- PencilMarkRedux/StyledMarkdown.swift
+++ PencilMarkRedux/StyledMarkdown.swift
@@ -12,7 +12,7 @@ import UniformTypeIdentifiers
 /**
  Based on [HWS Tutorial](https://www.hackingwithswift.com/quick-start/swiftui/how-to-create-a-document-based-app-using-filedocument-and-documentgroup)
  */
-struct StyledMarkdown: FileDocument {
+struct StyledMarkdown {
     /// tell the system we support only plain text
     static var readableContentTypes = [UTType.plainText]
     
@@ -26,23 +26,6 @@ struct StyledMarkdown: FileDocument {
         self.styledText = styledMarkdown(from: text)
         self.ast = Parser.shared.parse(markdown: text)
     }
-
-    /// Loads data that has been saved previously.
-    init(configuration: ReadConfiguration) throws {
-        if let data = configuration.file.regularFileContents {
-            text = String(decoding: data, as: UTF8.self)
-            styledText = styledMarkdown(from: text)
-            ast = Parser.shared.parse(markdown: text)
-        } else {
-            throw CocoaError(.fileReadCorruptFile)
-        }
-    }
-    
-    /// Called when the system wants to write data to disk.
-    func fileWrapper(configuration: WriteConfiguration) throws -> FileWrapper {
-        let data = Data(text.utf8)
-        return FileWrapper(regularFileWithContents: data)
-    }
 }
 
 extension StyledMarkdown {
@@ -50,3 +33,4 @@ extension StyledMarkdown {
         text[node.position.nsRange.lowerBound..<node.position.nsRange.upperBound]
     }
 }
+
