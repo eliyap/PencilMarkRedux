d9c143077d9f2340970d907fd013ac67bbc9f70f
Ipsky <yap@usc.edu>
2021-07-12 12:47:37 -0700

    fix intersection

--- Extensions/NSRange_Intersects.swift
+++ Extensions/NSRange_Intersects.swift
@@ -8,7 +8,29 @@
 import Foundation
 
 extension NSRange {
+    /**
+     Intersection cases
+     ```
+     A intersects B from the left:
+     a---A
+      b---B
+     
+     A intersects B from the right:
+      a---A
+     b---B
+     
+     A encloses B:
+     a-----A
+       b-B
+     
+     A is enclosed by B
+       a-A
+     b-----B
+     
+     always true that A>b, B>a
+     ```
+     */
     func intersects(with other: NSRange) -> Bool {
-        return (other.lowerBound <= lowerBound && lowerBound <= other.upperBound) || (other.lowerBound <= upperBound && upperBound <= other.upperBound)
+        return other.lowerBound < upperBound && other.upperBound > lowerBound
     }
 }
