c6e63e201569d6450d4297926fe0ec3223ca7b0c
Ipsky <yap@usc.edu>
2021-07-12 07:16:02 -0700

    define files

---
+++ PencilMarkRedux/LineStyle.swift
@@ -0,0 +1,12 @@
+//
+//  LineStyle.swift
+//  PencilMarkRedux
+//
+//  Created by Secret Asian Man Dev on 12/7/21.
+//
+
+import Foundation
+
+func lineStyle<T: Node>(style: T.Type) -> Void {
+    
+}
---
+++ PencilMarkRedux/StyledMarkdown.swift
@@ -0,0 +1,43 @@
+//
+//  StyledMarkdown.swift
+//  PencilMarkRedux
+//
+//  Created by Secret Asian Man Dev on 12/7/21.
+//
+
+import Foundation
+import SwiftUI
+import UniformTypeIdentifiers
+
+/**
+ Based on [HWS Tutorial](https://www.hackingwithswift.com/quick-start/swiftui/how-to-create-a-document-based-app-using-filedocument-and-documentgroup)
+ */
+struct StyledMarkdown: FileDocument {
+    /// tell the system we support only plain text
+    static var readableContentTypes = [UTType.plainText]
+    
+    var text: String
+    var styledText: NSMutableAttributedString
+    
+    /// a simple initializer that creates new, empty documents
+    init(text: String = "") {
+        self.text = text
+        self.styledText = styledMarkdown(from: text)
+    }
+
+    /// Loads data that has been saved previously.
+    init(configuration: ReadConfiguration) throws {
+        if let data = configuration.file.regularFileContents {
+            text = String(decoding: data, as: UTF8.self)
+            self.styledText = styledMarkdown(from: text)
+        } else {
+            throw CocoaError(.fileReadCorruptFile)
+        }
+    }
+    
+    /// Called when the system wants to write data to disk.
+    func fileWrapper(configuration: WriteConfiguration) throws -> FileWrapper {
+        let data = Data(text.utf8)
+        return FileWrapper(regularFileWithContents: data)
+    }
+}
