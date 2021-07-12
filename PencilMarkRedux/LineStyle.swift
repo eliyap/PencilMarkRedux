b28b29bdfa925905d894ab5f032c0a6714f8aae4
Ipsky <yap@usc.edu>
2021-07-12 07:35:24 -0700

    get intersecting leaves

--- PencilMarkRedux/LineStyle.swift
+++ PencilMarkRedux/LineStyle.swift
@@ -6,7 +6,44 @@
 //
 
 import Foundation
+import UIKit
 
-func lineStyle<T: Node>(style: T.Type) -> Void {
+extension StyledMarkdown {
+    func apply<T: Node>(lineStyle: T.Type, to range: NSRange) -> Void {
+        
+    }
+}
+
+extension Node {
     
+    func intersectingText(in range: NSRange) -> [Text] {
+        intersectingLeaves(in: range)
+            /// Filter out non text nodes and warn me about them.
+            .compactMap { (node: Node) -> Text? in
+                if let text = node as? Text {
+                    return text
+                } else {
+                    print("Intersected non text node: \(node)")
+                    return nil
+                }
+            }
+    }
+    
+    /// Get all leaf nodes in the AST which intersect the provided range.
+    func intersectingLeaves(in range: NSRange) -> [Node] {
+        /// If this does not intersect, none of its children will either
+        guard range.lowerBound < position.nsRange.upperBound || range.upperBound > position.nsRange.lowerBound else {
+            return []
+        }
+        
+        if children.isEmpty {
+            /// This is a leaf node (with no children).
+            return [self]
+        } else {
+            /// Combine results from node children.
+            return children
+                .compactMap { $0 as? Node }
+                .flatMap { $0.intersectingLeaves(in: range) }
+        }
+    }
 }
