7f49ab261a594520801fe29d4616f72d91537e02
Ipsky <yap@usc.edu>
2021-07-15 12:38:44 -0700

    copy over stroke interpretation code

---
+++ GestureRecognition/PMShape.swift
@@ -0,0 +1,14 @@
+//
+//  PMShape.swift
+//  PencilMarkRedux
+//
+//  Created by Secret Asian Man Dev on 15/7/21.
+//
+
+import Foundation
+
+/// Types of shapes recognized by PencilMark
+enum PMShape {
+    case wavyLine
+    case horizontalLine
+}
