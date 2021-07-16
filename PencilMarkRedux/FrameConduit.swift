7d07e4ed9163d1e2b3ae1519472431e84a0241a4
Ipsky <yap@usc.edu>
2021-07-16 12:32:11 -0700

    add another conduit

---
+++ PencilMarkRedux/FrameConduit.swift
@@ -0,0 +1,14 @@
+//
+//  FrameConduit.swift
+//  PencilMarkRedux
+//
+//  Created by Secret Asian Man Dev on 16/7/21.
+//
+
+import Foundation
+import CoreGraphics
+
+/// Combine conduit for passing view frame `CGRect`s between `UIKit` views.
+final class FrameConduit: ObservableObject {
+    @Published var size: CGRect? = nil
+}
