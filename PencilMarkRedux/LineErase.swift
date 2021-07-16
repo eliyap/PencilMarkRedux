3c29099fad55b1c891388b9adb247fa7153ef762
Ipsky <yap@usc.edu>
2021-07-16 06:37:53 -0700

    break off function

--- PencilMarkRedux/LineErase.swift
+++ PencilMarkRedux/LineErase.swift
@@ -14,41 +14,47 @@ extension StyledMarkdown {
         guard range.length > 0 else { return }
         let intersected: [Text] = ast.intersectingText(in: range)
         
-        for textNode in intersected {
-            /**
-             Compare the targeted substring, and the text node contents, ignoring surrounding whitespace.
-             If equal, then the whole node can be removed.
-             */
-            let intersection = textNode.position.nsRange.intersection(with: range)
-            let trimmedTarget = text[intersection].trimmingCharacters(in: .whitespaces)
-            let trimmedWhole = text[textNode.position.nsRange].trimmingCharacters(in: .whitespaces)
-            if trimmedWhole == trimmedTarget {
-                textNode._change = .toRemove
-            } else {
-                let (prefix, middle, suffix) = textNode.split(on: range)
-                
-                /// point split nodes to parent
-                prefix?.parent = textNode.parent
-                middle.parent = textNode.parent
-                suffix?.parent = textNode.parent
-                
-                /// mark target for removal
-                middle._change = .toRemove
-                
-                /// remove `nil` nodes
-                let pieces: [Text] = [prefix, middle, suffix].compactMap{ $0 }
-                
-                /// replace self in parent's children
-                textNode.parent.children.replaceSubrange(textNode.indexInParent..<(textNode.indexInParent+1), with: pieces)
-                
-                /// finally, remove parent pointer, should now be de-allocated
-                textNode.parent = nil
-            }
-        }
+        /// Mark changes in AST
+        intersected.forEach { $0.erase(in: range, in: self) }
         
         #warning("Todo: port infected tree code!")
         #warning("Todo: run some form of `consume`!")
         
         makeReplacements()
+    }
+}
+
+extension Text {
+    /// Mark changes in the AST needed to erase the passed range.
+    func erase(in range: NSRange, in document: StyledMarkdown) -> Void {
+        /**
+         Compare the targeted substring, and the text node contents, ignoring surrounding whitespace.
+         If equal, then the whole node can be removed.
+         */
+        let intersection = position.nsRange.intersection(with: range)
+        let trimmedTarget = document.text[intersection].trimmingCharacters(in: .whitespaces)
+        let trimmedWhole = document.text[position.nsRange].trimmingCharacters(in: .whitespaces)
+        if trimmedWhole == trimmedTarget {
+            _change = .toRemove
+        } else {
+            let (prefix, middle, suffix) = self.split(on: range)
+            
+            /// point split nodes to parent
+            prefix?.parent = parent
+            middle.parent = parent
+            suffix?.parent = parent
+            
+            /// mark target for removal
+            middle._change = .toRemove
+            
+            /// remove `nil` nodes
+            let pieces: [Text] = [prefix, middle, suffix].compactMap{ $0 }
+            
+            /// replace self in parent's children
+            parent.children.replaceSubrange(indexInParent..<(indexInParent+1), with: pieces)
+            
+            /// finally, remove parent pointer, should now be de-allocated
+            parent = nil
+        }
     }
 }
