83fd9b9a0287eec4a47691d3b7ab24a3a95c23ae
Ipsky <yap@usc.edu>
2021-07-18 19:42:24 -0700

    fix tests with sock puppet text view

--- PencilMarkReduxTests/LineEraseTests.swift
+++ PencilMarkReduxTests/LineEraseTests.swift
@@ -21,35 +21,39 @@ class LineEraseTests: XCTestCase {
     /// make sure zero length ranges are rejected
     func testZeroRange() throws {
         var document = StyledMarkdown()
+        let textView = PMTextView() /// dummy view to satisfy function signature
         
         document = StyledMarkdown(text: "aaa")
-        document.erase(in: NSMakeRange(1, 0))
+        document.erase(to: NSMakeRange(1, 0), in: textView)
         XCTAssertEqual(document.text, "aaa")
     }
 
     /// Basic erase of part of a string
     func testMid() throws {
         var document = StyledMarkdown()
+        let textView = PMTextView() /// dummy view to satisfy function signature
         
         document = StyledMarkdown(text: "aDELETEa")
-        document.erase(in: NSMakeRange(1, 6))
+        document.erase(to: NSMakeRange(1, 6), in: textView)
         XCTAssertEqual(document.text, "aa")
     }
     
     /// Erasing part of a phrasing block, make sure the phrasing block isn't broken
     func testPartial() throws {
         var document = StyledMarkdown()
+        let textView = PMTextView() /// dummy view to satisfy function signature
         
         document = StyledMarkdown(text: "_aaBB_ BBaa")
-        document.erase(in: NSMakeRange(3, 6)) /// target 'BB_ BB'
+        document.erase(to: NSMakeRange(3, 6), in: textView) /// target 'BB_ BB'
         XCTAssertEqual(document.text, "_aa_aa")
     }
     
     func testComplete() throws {
         var document = StyledMarkdown()
+        let textView = PMTextView() /// dummy view to satisfy function signature
         
         document = StyledMarkdown(text: "a _BBB_ a")
-        document.erase(in: NSMakeRange(3, 3)) /// target 'BBB'
+        document.erase(to: NSMakeRange(3, 3), in: textView) /// target 'BBB'
         XCTExpectFailure("Haven't implemented smart whitespace removal") {
             XCTAssertEqual(document.text, "a a") /// is actually "a  a" (two spaces)
         }
@@ -58,33 +62,34 @@ class LineEraseTests: XCTestCase {
     /// test deep nesting of phrasing blocks
     func testNestedPhrasing() throws {
         var document = StyledMarkdown()
+        let textView = PMTextView() /// dummy view to satisfy function signature
         
         document = StyledMarkdown(text: "_BBB_")
-        document.erase(in: NSMakeRange(1, 3)) /// target 'BBB'
+        document.erase(to: NSMakeRange(1, 3), in: textView) /// target 'BBB'
         XCTAssertEqual(document.text, "")
         
         document = StyledMarkdown(text: "**~~_BBB_~~**")
-        document.erase(in: NSMakeRange(5, 3)) /// target 'BBB'
+        document.erase(to: NSMakeRange(5, 3), in: textView) /// target 'BBB'
         XCTAssertEqual(document.text, "")
         
         /// try multiple nested phrasing blocks
         document = StyledMarkdown(text: "**~~_BBB_~~ ~~_BBB_~~ _BBB_**")
-        document.erase(in: NSMakeRange(5, 21)) /// target 'BBB_~~ ~~_BBB_~~ _BBB'
+        document.erase(to: NSMakeRange(5, 21), in: textView) /// target 'BBB_~~ ~~_BBB_~~ _BBB'
         XCTAssertEqual(document.text, "")
         
         /// check simple Headings
         document = StyledMarkdown(text: "# BBB")
-        document.erase(in: NSMakeRange(2, 3)) /// target 'BBB'
+        document.erase(to: NSMakeRange(2, 3), in: textView) /// target 'BBB'
         XCTAssertEqual(document.text, "")
         
         /// check styled headings
         document = StyledMarkdown(text: "# ~~*BBB*~~")
-        document.erase(in: NSMakeRange(5, 3)) /// target 'BBB'
+        document.erase(to: NSMakeRange(5, 3), in: textView) /// target 'BBB'
         XCTAssertEqual(document.text, "")
         
         /// Check headings with extra whitespace
         document = StyledMarkdown(text: "###   BBB")
-        document.erase(in: NSMakeRange(6, 3)) /// target 'BBB'
+        document.erase(to: NSMakeRange(6, 3), in: textView) /// target 'BBB'
         XCTAssertEqual(document.text, "")
         
         /// Check Setext style headings
@@ -92,7 +97,7 @@ class LineEraseTests: XCTestCase {
             BBB
             ===
             """)
-        document.erase(in: NSMakeRange(0, 3)) /// target 'BBB'
+        document.erase(to: NSMakeRange(0, 3), in: textView) /// target 'BBB'
         XCTAssertEqual(document.text, "")
         
     }
@@ -100,15 +105,16 @@ class LineEraseTests: XCTestCase {
     /// test nesting of list items
     func testNestedLists() throws {
         var document = StyledMarkdown()
+        let textView = PMTextView() /// dummy view to satisfy function signature
         
         /// simple list item check
         document = StyledMarkdown(text: "- BBB")
-        document.erase(in: NSMakeRange(2, 3)) /// target 'BBB'
+        document.erase(to: NSMakeRange(2, 3), in: textView) /// target 'BBB'
         XCTAssertEqual(document.text, "")
         
         /// nested list check
         document = StyledMarkdown(text: "- - BBB")
-        document.erase(in: NSMakeRange(4, 3)) /// target 'BBB'
+        document.erase(to: NSMakeRange(4, 3), in: textView) /// target 'BBB'
         XCTAssertEqual(document.text, "")
         
         /// multi-item list check
@@ -116,19 +122,19 @@ class LineEraseTests: XCTestCase {
             - BBB
             - aaa
             """)
-        document.erase(in: NSMakeRange(2, 3)) /// target 'BBB'
+        document.erase(to: NSMakeRange(2, 3), in: textView) /// target 'BBB'
         XCTExpectFailure("Haven't implemented list removal") {
             XCTAssertEqual(document.text, "- aaa") /// includes extra newline
         }
         
         /// list with leading spaces
         document = StyledMarkdown(text: "-   BBB")
-        document.erase(in: NSMakeRange(4, 3)) /// target 'BBB'
+        document.erase(to: NSMakeRange(4, 3), in: textView) /// target 'BBB'
         XCTAssertEqual(document.text, "")
         
         /// list with phrasing formatting
         document = StyledMarkdown(text: "- ~~**BBB**~~")
-        document.erase(in: NSMakeRange(6, 3)) /// target 'BBB'
+        document.erase(to: NSMakeRange(6, 3), in: textView) /// target 'BBB'
         XCTAssertEqual(document.text, "")
     }
 }
--- PencilMarkReduxTests/PhrasingTests.swift
+++ PencilMarkReduxTests/PhrasingTests.swift
@@ -21,58 +21,62 @@ class PhrasingTests: XCTestCase {
     /// make sure zero length ranges are rejected
     func testZeroRange() throws {
         var document = StyledMarkdown()
+        let textView = PMTextView() /// dummy view to satisfy function signature
         
         document = StyledMarkdown(text: "aaa")
-        document.apply(lineStyle: Delete.self, to: NSMakeRange(1, 0))
+        document.apply(lineStyle: Delete.self, to: NSMakeRange(1, 0), in: textView)
         XCTAssertEqual(document.text, "aaa")
     }
 
     /// Formats through the middle of a line
     func testMid() throws {
         var document = StyledMarkdown()
+        let textView = PMTextView() /// dummy view to satisfy function signature
         
         /// ~~DELETE~~
         document = StyledMarkdown(text: "aSTRIKEa")
-        document.apply(lineStyle: Delete.self, to: NSMakeRange(1, 6))
+        document.apply(lineStyle: Delete.self, to: NSMakeRange(1, 6), in: textView)
         XCTAssertEqual(document.text, "a~~STRIKE~~a")
         
         /// **STRONG**
         document = StyledMarkdown(text: "aSTRIKEa")
-        document.apply(lineStyle: Strong.self, to: NSMakeRange(1, 6))
+        document.apply(lineStyle: Strong.self, to: NSMakeRange(1, 6), in: textView)
         XCTAssertTrue(document.text == "a**STRIKE**a" || document.text == "a__STRIKE__a")
         
         /// *EMPHASIS*
         document = StyledMarkdown(text: "aSTRIKEa")
-        document.apply(lineStyle: Emphasis.self, to: NSMakeRange(1, 6))
+        document.apply(lineStyle: Emphasis.self, to: NSMakeRange(1, 6), in: textView)
         XCTAssertTrue(document.text == "a*STRIKE*a" || document.text == "a_STRIKE_a")
     }
     
     /// Strikes an entire phrasing element and its surroundings
     func testEnclosing() throws {
         var document = StyledMarkdown()
+        let textView = PMTextView() /// dummy view to satisfy function signature
         
         /// ~~DELETE~~
         document = StyledMarkdown(text: "aS _SSS_ Sa")
-        document.apply(lineStyle: Delete.self, to: NSMakeRange(1, 9))
+        document.apply(lineStyle: Delete.self, to: NSMakeRange(1, 9), in: textView)
         XCTAssertEqual(document.text, "a~~S _SSS_ S~~a")
         
         /// **STRONG**
         document = StyledMarkdown(text: "aS _SSS_ Sa")
-        document.apply(lineStyle: Strong.self, to: NSMakeRange(1, 9))
+        document.apply(lineStyle: Strong.self, to: NSMakeRange(1, 9), in: textView)
         XCTAssertTrue(document.text == "a**S _SSS_ S**a" || document.text == "a__S _SSS_ S__a")
         
         /// *EMPHASIS*
         document = StyledMarkdown(text: "aS **SSS** Sa")
-        document.apply(lineStyle: Emphasis.self, to: NSMakeRange(1, 11))
+        document.apply(lineStyle: Emphasis.self, to: NSMakeRange(1, 11), in: textView)
         XCTAssertTrue(document.text == "a*S **SSS** S*a" || document.text == "a_S **SSS** S_a")
     }
     
     /// Strikes partly inside, partly outside a phrasing block
     func testPartial() throws {
         var document = StyledMarkdown()
+        let textView = PMTextView() /// dummy view to satisfy function signature
         
         document = StyledMarkdown(text: "_aaBB_ BBaa")
-        document.apply(lineStyle: Delete.self, to: NSMakeRange(3, 6))
+        document.apply(lineStyle: Delete.self, to: NSMakeRange(3, 6), in: textView)
         XCTAssertEqual(document.text, "_aa~~BB~~_ ~~BB~~aa") /// note ejection of leading whitespace
         
         #warning("TODO: add other phasing content")
@@ -81,26 +85,28 @@ class PhrasingTests: XCTestCase {
     /// test whether leading whitespace is ejected correctly
     func testEjectLeadingWhitespace() throws {
         var document = StyledMarkdown()
+        let textView = PMTextView() /// dummy view to satisfy function signature
         
         document = StyledMarkdown(text: "AAA BBB")
-        document.apply(lineStyle: Delete.self, to: NSMakeRange(3, 4)) /// target ' BBB'
+        document.apply(lineStyle: Delete.self, to: NSMakeRange(3, 4), in: textView) /// target ' BBB'
         XCTAssertEqual(document.text, "AAA ~~BBB~~")
         
         document = StyledMarkdown(text: "AAA *BBB*")
-        document.apply(lineStyle: Delete.self, to: NSMakeRange(3, 5)) /// target ' *BBB'
+        document.apply(lineStyle: Delete.self, to: NSMakeRange(3, 5), in: textView) /// target ' *BBB'
         XCTAssertEqual(document.text, "AAA *~~BBB~~*")
     }
     
     /// test whether trailing whitespace is ejected correctly
     func testEjectTrailingWhitespace() throws {
         var document = StyledMarkdown()
+        let textView = PMTextView() /// dummy view to satisfy function signature
         
         document = StyledMarkdown(text: "AAA BBB")
-        document.apply(lineStyle: Delete.self, to: NSMakeRange(0, 4)) /// target 'AAA '
+        document.apply(lineStyle: Delete.self, to: NSMakeRange(0, 4), in: textView) /// target 'AAA '
         XCTAssertEqual(document.text, "~~AAA~~ BBB")
         
         document = StyledMarkdown(text: "*AAA* BBB")
-        document.apply(lineStyle: Delete.self, to: NSMakeRange(1, 5)) /// target 'AAA* '
+        document.apply(lineStyle: Delete.self, to: NSMakeRange(1, 5), in: textView) /// target 'AAA* '
         XCTAssertEqual(document.text, "*~~AAA~~* BBB")
     }
     
@@ -108,9 +114,10 @@ class PhrasingTests: XCTestCase {
     /// Expect it to unwrap that enclosed block.
     func testUnwrap() throws {
         var document = StyledMarkdown()
+        let textView = PMTextView() /// dummy view to satisfy function signature
         
         document = StyledMarkdown(text: "aS _~~SSS~~_ Sa")
-        document.apply(lineStyle: Delete.self, to: NSMakeRange(1, 13))
+        document.apply(lineStyle: Delete.self, to: NSMakeRange(1, 13), in: textView)
         XCTAssertEqual(document.text, "a~~S _SSS_ S~~a")
         
         #warning("TODO: add other phasing content")
@@ -118,17 +125,19 @@ class PhrasingTests: XCTestCase {
     
     func testExtensionLeading() throws {
         var document = StyledMarkdown()
+        let textView = PMTextView() /// dummy view to satisfy function signature
         
         document = StyledMarkdown(text: "~~AAA BBB~~ CCC")
-        document.apply(lineStyle: Delete.self, to: NSMakeRange(6, 9)) /// strike `BBB~~ CCC`
+        document.apply(lineStyle: Delete.self, to: NSMakeRange(6, 9), in: textView) /// strike `BBB~~ CCC`
         XCTAssertEqual(document.text, "~~AAA BBB CCC~~")
     }
     
     func testExtensionTrailing() throws {
         var document = StyledMarkdown()
+        let textView = PMTextView() /// dummy view to satisfy function signature
         
         document = StyledMarkdown(text: "AAA ~~BBB CCC~~")
-        document.apply(lineStyle: Delete.self, to: NSMakeRange(0, 9)) /// strike `AAA ~~BBB`
+        document.apply(lineStyle: Delete.self, to: NSMakeRange(0, 9), in: textView) /// strike `AAA ~~BBB`
         XCTAssertEqual(document.text, "~~AAA BBB CCC~~")
     }
 }
