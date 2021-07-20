dc40163ad026034af9d4cb96d0daf1c315f85156
Ipsky <yap@usc.edu>
2021-07-20 15:38:16 -0700

    hide and show nav bar

--- PencilMarkRedux/ContentView.swift
+++ PencilMarkRedux/ContentView.swift
@@ -14,19 +14,33 @@ struct ContentView: View {
     let cmdC = CommandConduit()
     @Binding var document: StyledMarkdown
     
+    @State var hideBar = false
+    
     var body: some View {
-        GeometryReader { geo in
-            HStack {
-                Spacer()
-                    .frame(maxWidth: geo.size.width * 0.1)
-                ZStack {
-                    KeyboardEditorView(strokeC: strokeC, frameC: frameC, cmdC: cmdC, document: $document)
-                    CanvasView(strokeC: strokeC, frameC: frameC, cmdC: cmdC)
+        ZStack(alignment: .bottomTrailing) {
+            GeometryReader { geo in
+                HStack {
+                    Spacer()
+                        .frame(maxWidth: geo.size.width * 0.1)
+                    ZStack {
+                        KeyboardEditorView(strokeC: strokeC, frameC: frameC, cmdC: cmdC, document: $document)
+                        CanvasView(strokeC: strokeC, frameC: frameC, cmdC: cmdC)
+                    }
+                    Spacer()
+                        .frame(maxWidth: geo.size.width * 0.1)
                 }
-                Spacer()
-                    .frame(maxWidth: geo.size.width * 0.1)
             }
+            Button { withAnimation { hideBar.toggle() } } label: {
+                SwiftUI.Image(systemName: hideBar
+                    ? "arrow.down.right.and.arrow.up.left.circle.fill"
+                    : "arrow.up.backward.and.arrow.down.forward.circle.fill"
+                )
+                    .font(.title)
+                    .foregroundColor(.secondary)
+            }
+                .padding()
         }
+            .navigationBarHidden(hideBar)
     }
 }
 
