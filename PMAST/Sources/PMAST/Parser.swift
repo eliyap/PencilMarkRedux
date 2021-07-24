57d81162e93c89e18c4f50602e9f5839a6af835a
Ipsky <yap@usc.edu>
2021-07-24 16:38:52 -0700

    import classes

---
+++ PMAST/Sources/PMAST/Parser.swift
@@ -0,0 +1,39 @@
+//
+//  MDAST.swift
+//  PencilMarkRedux
+//
+//  Created by Secret Asian Man Dev on 11/7/21.
+//
+
+import Foundation
+import JavaScriptCore
+
+final internal class Parser {
+    
+    static let shared = Parser()
+    
+    let context: JSContext = JSContext()!
+    let url: URL = Bundle.main.url(forResource: "main", withExtension: "js")!
+    
+    init() {
+        context.evaluateScript(try! String(contentsOf: url), withSourceURL: url)
+    }
+    
+    func parse(markdown: String) -> Root {
+        let result = context
+            .objectForKeyedSubscript("PMJS")
+            .objectForKeyedSubscript("parse")
+            .call(withArguments: [markdown])
+        
+        #warning("Unsafe Unwrap!")
+        let dict = result!.toDictionary()!
+        
+        let ast = Root(dict: dict, parent: nil)!
+        
+        /// assert tree is ok
+        try! ast.linkCheck()
+        
+        return ast
+        
+    }
+}
