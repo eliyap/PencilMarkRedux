f932549586d4bd2e1f75d47d71c17a7214d9216d
Ipsky <yap@usc.edu>
2021-07-24 11:10:31 -0700

    Revert "comment"
    This reverts commit 707f61df5408fbb17e09c50dc1d9b55d7a56347b.

--- Extensions/UIViewController/UIViewController.adopt.swift
+++ Extensions/UIViewController/UIViewController.adopt.swift
@@ -9,7 +9,6 @@ import Foundation
 import UIKit
 
 extension UIViewController {
-    /// Convenience method for adding a subview to the view hierarchy.
     func adopt(_ child: ViewController) {
         addChild(child)
         child.view.frame = view.frame
