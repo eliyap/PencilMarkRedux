6b675c955b82f650ee78a91c1e93bc7ed938c369
Ipsky <yap@usc.edu>
2021-07-24 11:13:54 -0700

    Revert "Revert "add adopt""
    This reverts commit 127f040913553d8d27201656badbb9bbf1b2535f.

--- Extensions/UIViewController/PMViewController.swift
+++ Extensions/UIViewController/PMViewController.swift
@@ -9,6 +9,7 @@ import Foundation
 import UIKit
 import Combine
 
+/// A `UIViewController` assumed to have `Combine` capabilities.
 class PMViewController: UIViewController {
     
     private var observers = Set<AnyCancellable>()
