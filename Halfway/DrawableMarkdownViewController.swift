c108d42b1a198d83c07cf28a7f4199606b45ed5c
Ipsky <yap@usc.edu>
2021-07-23 18:38:37 -0700

    copy in document code

--- Halfway/DrawableMarkdownViewController.swift
+++ Halfway/DrawableMarkdownViewController.swift
@@ -19,6 +19,15 @@ final class DrawableMarkdownViewController: UIViewController {
 //    let cmdC = CommandConduit()
 //    let document: StyledMarkdown
     
+    /// Nullable underlying model object
+    private var _document: TextDocument?
+    
+    /// Non-nullable public model object
+    public var document: TextDocument {
+        get { getDocument() }
+        set { setDocument(to: newValue) }
+    }
+    
     init(url: URL) {
         self.url = url
         super.init(nibName: nil, bundle: nil)
@@ -26,5 +35,50 @@ final class DrawableMarkdownViewController: UIViewController {
     
     required init?(coder: NSCoder) {
         fatalError("Do Not use")
+    }
+    
+    /// Returns the underlying document, if any,
+    /// or creates a new one if there isn't.
+    private func getDocument() -> TextDocument {
+        if let _document = _document {
+            return _document
+        } else {
+            /// Create new document here.
+            let data = "".data(using: .utf8)! /// Initialize with no text
+            let newURL = url.appendingPathComponent("Untitled.txt") /// Default title
+            try! data.write(to: newURL)
+            
+            print("New Document Created")
+            
+            _document = TextDocument(fileURL: newURL)
+            return _document!
+        }
+    }
+    
+    /// Updates the document being displayed,
+    /// first closing the old document, if any.
+    private func setDocument(to document: TextDocument) -> Void {
+        
+        func open(new document: TextDocument) {
+            self._document = document
+            document.open { (success) in
+                #warning("Set Document Here")
+//                self.textView.text = self.document.text
+            }
+        }
+        
+        /// Close existing document, if any, then open the new one.
+        if let _document = _document {
+            print("Text was: " + _document.text)
+            _document.close { (success) in
+                if success == false {
+                    print("Failed to close document!")
+                } else {
+                    open(new: document)
+                }
+            }
+        } else {
+            open(new: document)
+        }
     }
 }
