ddf61b40d698759434feb9b3c2e186f99b5fb5df
Ipsky <yap@usc.edu>
2021-07-23 18:13:04 -0700

    create shell of new VC

---
+++ Imported/DrawableMarkdownViewController.swift
@@ -0,0 +1,25 @@
+//
+//  DrawableMarkdownViewController.swift
+//  PencilMarkRedux
+//
+//  Created by Secret Asian Man Dev on 23/7/21.
+//
+
+import Foundation
+import UIKit
+import Combine
+
+final class DrawableMarkdownViewController: UIViewController {
+//    let strokeC = StrokeConduit()
+//    let frameC = FrameConduit()
+//    let cmdC = CommandConduit()
+//    let document: StyledMarkdown
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("Do Not use")
+    }
+}
