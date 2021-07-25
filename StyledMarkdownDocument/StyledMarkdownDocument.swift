978ab15fe59e6355601c1623200771fca335a0a3
Ipsky <yap@usc.edu>
2021-07-24 20:19:35 -0700

    remove styling methods

--- StyledMarkdownDocument/StyledMarkdownDocument.swift
+++ StyledMarkdownDocument/StyledMarkdownDocument.swift
@@ -52,28 +52,3 @@ final class StyledMarkdownDocument: UIDocument {
         fileURL.lastPathComponent
     }
 }
-
-// MARK:- Styling Methods
-extension StyledMarkdownDocument {
-    /// Call this function to update after the text is updated.
-    func updateAttributes() -> Void {
-        /// re-formulate AST
-        ast = Parser.shared.parse(markdown: text)
-        
-        /// re-format string based on AST
-        styledText = Self.attributedText(from: text, with: ast)
-    }
-    
-    /// Uses the AST to style an attributed string
-    /// - Note: make sure the passed AST matches the passed text!
-    static func attributedText(from markdown: String, with ast: Root) -> NSMutableAttributedString {
-        var result = NSMutableAttributedString(
-            string: markdown, attributes: [
-                /// set font to monospace by default
-                .font: UIFont.monospacedSystemFont(ofSize: UIFont.systemFontSize, weight: .regular)
-            ]
-        )
-        ast.style(&result)
-        return result
-    }
-}
