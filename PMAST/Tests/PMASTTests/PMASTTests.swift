de63a7f1a60f1a525961282adffa426d2b24ca35
Ipsky <yap@usc.edu>
2021-07-24 17:02:40 -0700

    quick test

--- PMAST/Tests/PMASTTests/PMASTTests.swift
+++ PMAST/Tests/PMASTTests/PMASTTests.swift
@@ -3,9 +3,6 @@
 
     final class PMASTTests: XCTestCase {
         func testExample() {
-            // This is an example of a functional test case.
-            // Use XCTAssert and related functions to verify your tests produce the correct
-            // results.
-            XCTAssertEqual(PMAST().text, "Hello, World!")
+            Parser.shared.parse(markdown: "")
         }
     }
