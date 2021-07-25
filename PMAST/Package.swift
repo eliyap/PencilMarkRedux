540e20eb48603be798239f2f438d46c5bd8e5136
Ipsky <yap@usc.edu>
2021-07-24 19:57:39 -0700

    add OrderedCollections dependency to package

--- PMAST/Package.swift
+++ PMAST/Package.swift
@@ -16,14 +16,16 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-collections", from: "0.0.4"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "PMAST",
-            dependencies: [],
+            dependencies: [
+                .product(name: "OrderedCollections", package: "swift-collections"),
+            ],
             exclude: [
                 "Javascript/env",
                 "Javascript/venv",
@@ -36,7 +38,10 @@ let package = Package(
         ),
         .testTarget(
             name: "PMASTTests",
-            dependencies: ["PMAST"],
+            dependencies: [
+                "PMAST",
+                .product(name: "OrderedCollections", package: "swift-collections"),
+            ],
             exclude: [
                 "Javascript/env",
                 "Javascript/venv",
