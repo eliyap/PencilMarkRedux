dede3109d80297b577e4d4e537304509dd8c6993
Ipsky <yap@usc.edu>
2021-07-25 14:36:19 -0700

    fix visual jumps

--- DrawableMarkdownViewController/CanvasViewController/CanvasViewController_UIScrollViewDelegate.swift
+++ DrawableMarkdownViewController/CanvasViewController/CanvasViewController_UIScrollViewDelegate.swift
@@ -8,6 +8,26 @@
 import UIKit
 
 extension CanvasViewController: UIScrollViewDelegate {
+    /**
+     Before scrolling begins, check if the text view has a different position.
+     If it does, defer to its position. This might happen because:
+     - the user used the keyboard to navigate around the `textView`.
+     - there's small glitch in the keyboard adjustment code.
+     
+     - Note: in my observation, this is only called when the user scrolls with a finger! It does not fire on programmatic scroll.
+     */
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        /// Calculate difference in scroll positions
+        let keyboardY = coordinator.keyboard.textView.contentOffset.y
+        let diff = scrollView.contentOffset.y - keyboardY
+        
+        /// If there is any non-trivial difference, defer to text position to prevent a visual jump
+        if abs(diff) > 1 {
+            print("Discrepancy in scroll position!")
+            scrollView.contentOffset.y = keyboardY
+        }
+    }
+    
     /// Set common scroll position if this is leading.
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         guard coordinator.scrollLead == .canvas else { return }
