89efcda23ff23693c88df039ffeccb0d31b1eba9
Ipsky <yap@usc.edu>
2021-07-25 06:25:12 -0700

    remove old code

--- Imported/DocumentSelectionDelegate.swift
+++ Imported/DocumentSelectionDelegate.swift
@@ -10,35 +10,11 @@ import UIKit
 
 /// Very small protocol that lets `FileBrowserViewController` set the document of the `DocumentViewController`.
 protocol DocumentSelectionDelegate: AnyObject {
-    func select(_ document: TextDocument) -> Void
-}
-
-extension DocumentViewController: DocumentSelectionDelegate {
-    func select(_ document: TextDocument) {
-        /// Update Naviagation Bar Title
-        navigationItem.title = document.localizedName
-        
-        self.document = document
-    }
-}
-
-/// Very small protocol that lets `FileBrowserViewController` set the document of the `DocumentViewController`.
-protocol _DocumentSelectionDelegate: AnyObject {
     associatedtype Document: UIDocument
     func select(_ document: Document) -> Void
 }
 
-//extension _DocumentViewController: _DocumentSelectionDelegate {
-//    typealias Document = StyledMarkdownDocument
-//    func select(_ document: StyledMarkdownDocument) {
-//        /// Update Naviagation Bar Title
-//        navigationItem.title = document.localizedName
-//
-//        self.document = document
-//    }
-//}
-
-extension DrawableMarkdownViewController: _DocumentSelectionDelegate {
+extension DrawableMarkdownViewController: DocumentSelectionDelegate {
     func select(_ document: StyledMarkdownDocument) {
         /// Update Naviagation Bar Title
         navigationItem.title = document.localizedName
