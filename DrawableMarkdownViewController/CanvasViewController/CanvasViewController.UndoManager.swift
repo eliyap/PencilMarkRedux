38b5eb949764794f5ea0daa0d10cf03e316cea8a
Ipsky <yap@usc.edu>
2021-07-30 10:07:44 -0700

    check with text view on undo / redo possibility

--- DrawableMarkdownViewController/CanvasViewController/CanvasViewController.UndoManager.swift
+++ DrawableMarkdownViewController/CanvasViewController/CanvasViewController.UndoManager.swift
@@ -15,6 +15,11 @@ extension CanvasViewController {
         /// Offer ability to reference parent
         /// - Note: not set at ``init``, do not access it until after ``init`` is complete.
         weak var  controller: CanvasViewController! = nil
+
+        /// Reference `textView` for undo / redo status.
+        /// Default to ``true`` in case something goes wrong so that it doesn't break.
+        override var canUndo: Bool { controller?.coordinator.keyboard.textView.undoManager?.canUndo ?? true }
+        override var canRedo: Bool { controller?.coordinator.keyboard.textView.undoManager?.canRedo ?? true }
         
         override func undo() {
             super.undo()
