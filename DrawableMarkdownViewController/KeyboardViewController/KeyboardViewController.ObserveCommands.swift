de162aade7531bb58319f9469439b9f3034ab534
Ipsky <yap@usc.edu>
2021-07-29 20:00:39 -0700

    Revert "switch to calling into coordinator undomanager"
    This reverts commit 2f9f3f840109f6adbffc030e29716557f6052ca5.

--- DrawableMarkdownViewController/KeyboardViewController/KeyboardViewController.ObserveCommands.swift
+++ DrawableMarkdownViewController/KeyboardViewController/KeyboardViewController.ObserveCommands.swift
@@ -12,15 +12,15 @@ extension KeyboardViewController {
     func observeCommands() -> Void {
         let undo: AnyCancellable = coordinator.cmdC.undo
             .sink { [weak self] in
-                print("Command, Can undo: " + String(describing: self?.coordinator.undoManager.canUndo))
-                self?.coordinator.undoManager.undo()
+                print("Command, Can undo: " + String(describing: self?.textView.undoManager?.canUndo))
+                self?.textView.undoManager?.undo()
             }
         store(undo)
         
         let redo: AnyCancellable = coordinator.cmdC.redo
             .sink { [weak self] in
-                print("Command, Can redo: " + String(describing: self?.coordinator.undoManager.canRedo))
-                self?.coordinator.undoManager.redo()
+                print("Command, Can redo: " + String(describing: self?.textView.undoManager?.canRedo))
+                self?.textView.undoManager?.redo()
             }
         store(redo)
     }
