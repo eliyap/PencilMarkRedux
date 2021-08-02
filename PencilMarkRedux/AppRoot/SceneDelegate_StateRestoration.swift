2db173013a070f0d586b974fbaf8d5fd9d77a9e9
Ipsky <yap@usc.edu>
2021-08-01 18:13:39 -0700

    monitor UserActivity events

--- PencilMarkRedux/AppRoot/SceneDelegate_StateRestoration.swift
+++ PencilMarkRedux/AppRoot/SceneDelegate_StateRestoration.swift
@@ -9,6 +9,15 @@ import UIKit
 
 extension SceneDelegate {
     func stateRestorationActivity(for scene: UIScene) -> NSUserActivity? {
-        scene.userActivity
+        print("Has user activity: \(scene.userActivity != nil)")
+        return scene.userActivity
+    }
+    
+    func scene(_ scene: UIScene, continue userActivity: NSUserActivity) {
+        print("Continuing scene")
+    }
+    
+    func scene(_ scene: UIScene, didFailToContinueUserActivityWithType userActivityType: String, error: Error) {
+        assert(false, "Failed to continue scene!")
     }
 }
