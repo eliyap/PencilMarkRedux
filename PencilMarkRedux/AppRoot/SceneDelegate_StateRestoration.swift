20e3645049b3a68e315ecdea6af93fb7081987ce
Ipsky <yap@usc.edu>
2021-08-01 17:48:36 -0700

    opt into NSUserActivity based restoration

---
+++ PencilMarkRedux/AppRoot/SceneDelegate_StateRestoration.swift
@@ -0,0 +1,14 @@
+//
+//  SceneDelegate_StateRestoration.swift
+//  SceneDelegate_StateRestoration
+//
+//  Created by Secret Asian Man Dev on 1/8/21.
+//
+
+import UIKit
+
+extension SceneDelegate {
+    func stateRestorationActivity(for scene: UIScene) -> NSUserActivity? {
+        scene.userActivity
+    }
+}
