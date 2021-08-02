60fcbe578864f78a6689dfbc420861a887d6f1d8
Ipsky <yap@usc.edu>
2021-08-01 18:07:11 -0700

    copy over backgrounding methods

--- PencilMarkRedux/AppRoot/SceneDelegate.swift
+++ PencilMarkRedux/AppRoot/SceneDelegate.swift
@@ -33,11 +33,17 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+        if let userActivity = window?.windowScene?.userActivity {
+            userActivity.becomeCurrent()
+        }
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
+        if let userActivity = window?.windowScene?.userActivity {
+            userActivity.resignCurrent()
+        }
     }
 
     func sceneWillEnterForeground(_ scene: UIScene) {
