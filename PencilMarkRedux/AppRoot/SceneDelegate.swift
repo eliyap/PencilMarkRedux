772f15caa83eeb4de848e82149ba8eaa95d23069
Ipsky <yap@usc.edu>
2021-08-01 17:57:13 -0700

    monitor user activity

--- PencilMarkRedux/AppRoot/SceneDelegate.swift
+++ PencilMarkRedux/AppRoot/SceneDelegate.swift
@@ -14,10 +14,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        // Determine the user activity from a new connection or from a session's state restoration.
+        let userActivity = connectionOptions.userActivities.first ?? session.stateRestorationActivity
+        print("User Activity is present: \(userActivity != nil)")
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+//        guard let _ = (scene as? UIWindowScene) else { return }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
