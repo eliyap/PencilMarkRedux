225e617e031e2070a006fc8ce01d022f9e030a83
Ipsky <yap@usc.edu>
2021-08-02 19:32:16 -0700

    log msg

--- PencilMarkRedux/AppRoot/ViewController.swift
+++ PencilMarkRedux/AppRoot/ViewController.swift
@@ -45,7 +45,7 @@ final class ViewController: UISplitViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        print("Has Window Scene: \(view.window?.windowScene != nil)")
+        print("ViewController Has Window Scene: \(view.window?.windowScene != nil)")
         view.window?.windowScene?.userActivity = NSUserActivity(activityType: "com.pencilmark.example")
     }
 }
