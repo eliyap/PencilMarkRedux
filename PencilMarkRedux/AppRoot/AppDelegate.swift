ffae7d99c4f26f1976bf1008b57231acab371113
Ipsky <yap@usc.edu>
2021-08-02 19:42:48 -0700

    make auto layout shut up

--- PencilMarkRedux/AppRoot/AppDelegate.swift
+++ PencilMarkRedux/AppRoot/AppDelegate.swift
@@ -11,8 +11,10 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    /// Override point for customization after application launch.
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        /// Debug setting: make auto layout can it
+        UserDefaults.standard.set(false, forKey: "_UIConstraintBasedLayoutLogUnsatisfiable")
         return true
     }
 
