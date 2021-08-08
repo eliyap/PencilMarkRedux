d05d9b5fe30fdb0d9f7727684b26961b2068c35b
Ipsky <yap@usc.edu>
2021-08-08 12:49:32 -0700

    comment

--- PMAST/Sources/PMAST/Types/InlineJoinable.swift
+++ PMAST/Sources/PMAST/Types/InlineJoinable.swift
@@ -10,5 +10,8 @@ import Foundation
 /// Elements that are inline, that can easily be joined together.
 /// e.g. `**a****b**` should be `**ab**`
 protocol InlineJoinable: Node {
+    /**
+     Simple initializer allows us to create parents explicitly for insertion into the tree.
+     */
     init(parent: Parent?, position: Position, _type: String)
 }
