88b78826171da88d3b59e1360bb37f885bfb8543
Ipsky <yap@usc.edu>
2021-08-10 12:50:41 -0700

    define tutorial VC

---
+++ TutorialMenuViewController.swift
@@ -0,0 +1,22 @@
+//
+//  TutorialMenuViewController.swift
+//  PencilMarkRedux
+//
+//  Created by Secret Asian Man Dev on 10/8/21.
+//
+
+import UIKit
+
+final class TutorialMenuViewController: UIViewController {
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        let label = UILabel()
+        label.text = "Not Implemented"
+        view = label
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("Do Not use")
+    }
+}
