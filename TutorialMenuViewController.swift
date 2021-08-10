9073da2e00a969aa8b5f90c9b877e0f9ae138d3c
Ipsky <yap@usc.edu>
2021-08-10 13:51:15 -0700

    draft enum based table view

--- TutorialMenuViewController.swift
+++ TutorialMenuViewController.swift
@@ -7,18 +7,50 @@
 
 import UIKit
 
-final class TutorialMenuViewController: UIViewController {
+enum Gesture: Int, CaseIterable {
+    case strike = 1
+    case scribble
+    
+    var name: String {
+        switch self {
+        case .strike:
+            return "Strike"
+        case .scribble:
+            return "Scribble"
+        }
+    }
+}
+
+final class TutorialMenuViewController: UITableViewController {
     
     init() {
-        super.init(nibName: nil, bundle: nil)
+        super.init(style: .plain)
         
-        #warning("Placeholder!")
-        let label = UILabel()
-        label.text = "Not Implemented"
-        view = label
+        tableView.dataSource = self
     }
     
     required init?(coder: NSCoder) {
         fatalError("Do Not use")
+    }
+}
+
+// MARK: - Data Source
+final class TutorialDataSource: NSObject, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        Gesture.allCases.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        let gesture = Gesture(rawValue: indexPath.row)
+        cell.textLabel?.text = gesture?.name
+        
+        return cell
     }
 }
