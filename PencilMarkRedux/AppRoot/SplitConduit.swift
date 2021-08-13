af32171821413115dcba94e89fd84856d825e1aa
Ipsky <yap@usc.edu>
2021-08-13 13:19:45 -0700

    set up flag with comments

--- PencilMarkRedux/AppRoot/SplitConduit.swift
+++ PencilMarkRedux/AppRoot/SplitConduit.swift
@@ -15,8 +15,18 @@ final class SplitConduit {
     /// Singleton object.
     static let shared = SplitConduit()
     
-    /// Updates when `UISplitViewController` shows / hides the primary view controller
-    let primaryColumnChange = PassthroughSubject<Void, Never>()
+    /** 21.08.13
+     Observed issue:
+     - `viewWillLayoutSubviews` should pass in updated `view` boundaries.
+     - However, for the `KeyboardViewController`, it did not when the `UISplitViewController` primary column was shown.
+     - However however, the `DrawableMarkdownViewController` did get updated bounds!
+     
+     So, we want to request an extra layout pass _only_ when the primary column has just been shown and we think this bug is about to occur.
+     If we do it all the time, scrolling and other features don't work!
+     
+     Toggle this var on when primary column is hidden, and off after the extra layout pass is completed.
+     */
+    var needLayoutKeyboard = false
     
     init(){}
 }
