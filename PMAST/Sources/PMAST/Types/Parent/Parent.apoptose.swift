4b9660e845372cfbe9fc21204ba17cfe0921adc5
Ipsky <yap@usc.edu>
2021-08-14 17:22:27 -0700

    fix typo

--- PMAST/Sources/PMAST/Types/Parent/Parent.apoptose.swift
+++ PMAST/Sources/PMAST/Types/Parent/Parent.apoptose.swift
@@ -9,7 +9,7 @@ import Foundation
 
 extension Parent {
     /// Similar to cell apoptosis, we destroy the node, allowing its desendants to escape.
-    /// Node should be deallocated after thsi call since all references to it are destroyed.
+    /// Node should be deallocated after this call since all references to it are destroyed.
     func apoptose() {
         children.forEach { $0.parent = parent }
         parent.children.replaceSubrange(indexInParent!..<(indexInParent! + 1), with: children)
