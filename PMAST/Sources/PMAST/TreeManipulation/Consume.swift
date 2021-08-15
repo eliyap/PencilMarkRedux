63f4f52ad86488edf0eb1e60e3be9243d02902e7
Ipsky <yap@usc.edu>
2021-08-14 17:22:10 -0700

    split function

--- PMAST/Sources/PMAST/TreeManipulation/Consume.swift
+++ PMAST/Sources/PMAST/TreeManipulation/Consume.swift
@@ -167,14 +167,3 @@ extension Parent {
         }
     }
 }
-
-extension Parent {
-    /// Similar to cell apoptosis, we destroy the node, allowing its desendants to escape.
-    /// Node should be deallocated after thsi call since all references to it are destroyed.
-    func apoptose() {
-        children.forEach { $0.parent = parent }
-        parent.children.replaceSubrange(indexInParent!..<(indexInParent! + 1), with: children)
-        children = []
-        parent = nil
-    }
-}
