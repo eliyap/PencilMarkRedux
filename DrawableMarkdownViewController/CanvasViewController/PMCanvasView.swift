b0e87b6ea6c4717ec5b0a47984273a699463a869
Ipsky <yap@usc.edu>
2021-08-16 17:47:36 -0700

    clone eraser events

--- DrawableMarkdownViewController/CanvasViewController/PMCanvasView.swift
+++ DrawableMarkdownViewController/CanvasViewController/PMCanvasView.swift
@@ -22,3 +22,46 @@ final class PMCanvasView: PKCanvasView {
         }
     }
 }
+
+// MARK: - UIResponder Methods
+extension PMCanvasView {
+    /** - Note: does not fire on finger scroll, but does fire if finger drags when view cannot scroll */
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesMoved(touches, with: event)
+        
+        guard let delegate = delegate as? CanvasViewController else {
+            assert(false, "Unexpected wrong controller!")
+            return
+        }
+        
+        guard
+            delegate.coordinator.tool == .eraser,
+            let touch = touches.first,
+            /// a perfectly perpendicular input indicates a finger is being used
+            touch.altitudeAngle != CGFloat.pi / 2
+        else {
+            return
+        }
+        
+        print("touch")
+//        eraserActive = true
+//
+//        /// correct for scrolling offset before sending
+//        var location = touch.preciseLocation(in: view)
+//        trackCircle(location: location)
+//
+//        location.y -= self.contentOffset.y
+//        eraserConduit.location = location
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesEnded(touches, with: event)
+        
+        /// reject end events that result from finger taps
+//        guard eraserActive else { return }
+//
+//        removeCircle()
+//        eraserConduit.location = .none
+//        eraserActive = false
+    }
+}
