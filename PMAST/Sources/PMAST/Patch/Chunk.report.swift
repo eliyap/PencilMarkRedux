116b3daaadba35670fcc6d8897f4356e95eef280
Ipsky <yap@usc.edu>
2021-08-23 08:12:00 -0700

    define diffable protocol

--- PMAST/Sources/PMAST/Patch/Chunk.report.swift
+++ PMAST/Sources/PMAST/Patch/Chunk.report.swift
@@ -7,6 +7,31 @@
 
 import Foundation
 
+protocol Diffable {
+    /// Print out your changes as desired here.
+    /// ``symbol`` is either `+` or `-` for insert and remove respectively.
+    ///
+    /// `offset`, `element`, `associatedWith`, from standard change parameters
+    /// Docs: https://developer.apple.com/documentation/swift/collectiondifference/change/insert_offset_element_associatedwith
+    static func report(offset: Int, element: Self, associatedWith: Int?, symbol: Character) -> Void
+}
+
+extension Chunk: Diffable {
+    /// `offset`, `element`, `associatedWith`, from standard change parameters: https://developer.apple.com/documentation/swift/collectiondifference/change/insert_offset_element_associatedwith
+    static func report(offset: Int, element: Self, associatedWith: Int?, symbol: Character) -> Void {
+        (element.startIndex..<element.endIndex).forEach { idx in
+            /// Format something like this:
+            /// (+) 12: "new line!"
+            print(
+                "   ", /// padding
+                "(\(symbol))",
+                "\(idx)".padding(toLength: 3, withPad: " ", startingAt: 0) + ":", /// make line length uniform up to 999 lines
+                "\"\(element[idx].string)\""
+            )
+        }
+    }
+}
+
 /// Custom debug printout.
 internal extension CollectionDifference where ChangeElement == Chunk {
     
@@ -17,24 +42,10 @@ internal extension CollectionDifference where ChangeElement == Chunk {
             print("Chunk Change: ")
             switch change {
             case .insert(let offset, let element, let associatedWith):
-                Self.report(offset: offset, element: element, associatedWith: associatedWith, symbol: "+")
+                Chunk.report(offset: offset, element: element, associatedWith: associatedWith, symbol: "+")
             case .remove(let offset, let element, let associatedWith):
-                Self.report(offset: offset, element: element, associatedWith: associatedWith, symbol: "-")
+                Chunk.report(offset: offset, element: element, associatedWith: associatedWith, symbol: "-")
             }
-        }
-    }
-    
-    /// `offset`, `element`, `associatedWith`, from standard change parameters: https://developer.apple.com/documentation/swift/collectiondifference/change/insert_offset_element_associatedwith
-    static func report(offset: Int, element: ChangeElement, associatedWith: Int?, symbol: Character) -> Void {
-        (element.startIndex..<element.endIndex).forEach { idx in
-            /// Format something like this:
-            /// (+) 12: "new line!"
-            print(
-                "   ", /// padding
-                "(\(symbol))",
-                "\(idx)".padding(toLength: 3, withPad: " ", startingAt: 0) + ":", /// make line length uniform up to 999 lines
-                "\"\(element[idx].string)\""
-            )
         }
     }
 }
