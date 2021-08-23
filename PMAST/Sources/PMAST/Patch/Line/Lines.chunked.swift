4fa8e915fcb1a1f702312515542119686a636ba2
Ipsky <yap@usc.edu>
2021-08-22 21:05:32 -0700

    consider each blank line to be its own chunk, making the chunks totally cover the document

--- PMAST/Sources/PMAST/Patch/Line/Lines.chunked.swift
+++ PMAST/Sources/PMAST/Patch/Line/Lines.chunked.swift
@@ -14,7 +14,6 @@ extension Array where Element == Line {
             .map { idx in
                 self[boundaries[idx-1]..<boundaries[idx]]
             }
-            .map(\.trimmed)
             .filter {
                 /// Discard empty chunks
                 $0.isEmpty == false
@@ -27,8 +26,12 @@ extension Array where Element == Line {
         indices.forEach { idx in
             let line = self[idx]
             
-            /// Skip blank lines,
-            guard line.string.isBlank == false else { return }
+            /// If line is blank, report it as a new chunk.
+            /// This makes diffing easier.
+            guard line.string.isBlank == false else {
+                boundaries.append(idx + 1)
+                return
+            }
             
             /// Check if preceding line (if any) is blank,
             /// and that the line could not be continuing a block (besides a fenced code block).
