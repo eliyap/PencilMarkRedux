8bc93f601ffdfba9c868c2aed30bb247a1928a8f
Ipsky <yap@usc.edu>
2021-08-22 20:59:16 -0700

    move file

--- PMAST/Sources/PMAST/Patch/Line/Lines.chunked.swift
+++ PMAST/Sources/PMAST/Patch/Line/Lines.chunked.swift
@@ -1,5 +1,5 @@
 //
-//  Markdown.chunked.swift
+//  Lines.chunked.swift
 //  PencilMarkRedux
 //
 //  Created by Secret Asian Man Dev on 22/8/21.
@@ -7,12 +7,12 @@
 
 import Foundation
 
-internal extension Markdown {
-    func getChunks(in lines: [Line]) -> [Chunk] {
-        let boundaries = findBoundaries(in: lines)
+extension Array where Element == Line {
+    internal func chunked() -> [Chunk] {
+        let boundaries = findBoundaries()
         return (1..<boundaries.count)
             .map { idx in
-                lines[boundaries[idx-1]..<boundaries[idx]]
+                self[boundaries[idx-1]..<boundaries[idx]]
             }
             .map(\.trimmed)
             .filter {
@@ -21,26 +21,23 @@ internal extension Markdown {
             }
     }
     
-    func findBoundaries(in lines: [Line]) -> [Boundary] {
+    fileprivate func findBoundaries() -> [Boundary] {
         var boundaries: [Array<Line>.Index] = []
         
-        (lines.startIndex..<lines.endIndex).forEach { idx in
-            let line = lines[idx]
+        indices.forEach { idx in
+            let line = self[idx]
             
             /// Skip blank lines,
             guard line.string.isBlank == false else { return }
             
             /// Check if preceding line (if any) is blank,
             /// and that the line could not be continuing a block (besides a fenced code block).
-            if idx == 0 || (lines[idx - 1].string.isBlank && (line.string.isPotentiallyContinuation == false)) {
+            if idx == 0 || (self[idx - 1].string.isBlank && (line.string.isPotentiallyContinuation == false)) {
                 boundaries.append(idx + 1) /// adjust for 1 indexing
             }
         }
         
-        print("Found \(boundaries)")
-        print("Actual \(ast.children.map(\.position.start.line))")
-        
         /// Cap the document at both ends.
-        return [0] + boundaries + [lines.endIndex]
+        return [0] + boundaries + [endIndex]
     }
 }
