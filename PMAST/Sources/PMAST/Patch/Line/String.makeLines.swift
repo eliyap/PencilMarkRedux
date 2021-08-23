b1147c39fae26c0a0dc0820834e3d0d4f96c0a9b
Ipsky <yap@usc.edu>
2021-08-22 19:09:18 -0700

    clone make lines method

---
+++ PMAST/Sources/PMAST/Patch/Line/String.makeLines.swift
@@ -0,0 +1,37 @@
+//
+//  File.swift
+//  
+//
+//  Created by Secret Asian Man Dev on 22/8/21.
+//
+
+import Foundation
+
+internal extension String {
+    /// Generate ``Line`` structs.
+    func makeLines() -> [Line] {
+        
+        var result: [Line] = []
+        
+        /// Docs: https://developer.apple.com/documentation/foundation/nsstring/1416774-enumeratesubstrings
+        func block(substring: String?, substringRange: NSRange, enclosingRange: NSRange, stop: UnsafeMutablePointer<ObjCBool>) -> Void {
+            guard let substring = substring else {
+                /// Unsure what conditions generate a `nil` substring, guess we find out?
+                assert(false, "Unexpected empty substring")
+                return
+            }
+            
+            /// Perform safety checks.
+            precondition(substring == self[Range(substringRange, in: self)!], "Inconsistent Substring!")
+            precondition(substring.filter(\.isNewline).count == 0, "Newline char found in Line!")
+            
+            result.append(Line(string: substring, substringNsRange: substringRange, enclosingNsRange: enclosingRange))
+        }
+        
+        /// Invoke with `block` as closure.
+        let nsString = NSString(string: self)
+        nsString.enumerateSubstrings(in: NSMakeRange(0, nsString.length), options: .byLines, using: block)
+        
+        return result
+    }
+}
