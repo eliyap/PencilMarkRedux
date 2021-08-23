e746f348e67bd2af1e06e3d7381084148910e75e
Ipsky <yap@usc.edu>
2021-08-22 18:57:45 -0700

    split event

---
+++ PMAST/Sources/PMAST/Event.swift
@@ -0,0 +1,25 @@
+//
+//  File.swift
+//  
+//
+//  Created by Secret Asian Man Dev on 22/8/21.
+//
+
+import Foundation
+
+/// Borrowed from UNIST, an event occurs when we enter / exit a node.
+public enum Event {
+    /// Entering a node.
+    case enter(point: Point, type: String)
+    
+    /// Describe a ``Node``'s contents.
+    /// Intended for use by ``Literal``s so we can inspect their ``value``.
+    case contents(String)
+    
+    /// Exiting a node.
+    case exit(point: Point, type: String)
+}
+
+extension Event: Equatable {
+    /// Automagical!
+}
