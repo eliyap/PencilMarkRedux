68720eab2ddc7a296b4eb6a294b53d5cd3071b60
Ipsky <yap@usc.edu>
2021-08-23 08:26:39 -0700

    graft inserted tree

--- PMAST/Sources/PMAST/Patch/Markdown.patch.swift
+++ PMAST/Sources/PMAST/Patch/Markdown.patch.swift
@@ -9,7 +9,6 @@ import Foundation
 
 extension Markdown {
     public mutating func patch(with new: String) {
-        #warning("Not Implemented!")
         let oldChunks = plain.makeLines().chunked()
         let newChunks = new.makeLines().chunked()
         
@@ -19,9 +18,16 @@ extension Markdown {
         diff.forEach { change in
             switch change {
             case .insert(let offset, let element, let associatedWith):
-                print("Contents: \(new.contents(of: element))")
                 let node = Parser.shared.parse(markdown: new.contents(of: element))
                 #warning("TODO: insert this into tree")
+                /// adjust node positions
+                
+                /// adjust following positions
+                /// insert node into tree structure
+                    /// locate insertion point
+                let insertionIndex = ast.children.firstIndex { $0.position.end.offset < element.lowerBound }
+                    ?? 0 /// assume start of tree if not found.
+                ast.graft(node, at: insertionIndex)
             case .remove(let offset, let element, let associatedWith):
                 break
             }
