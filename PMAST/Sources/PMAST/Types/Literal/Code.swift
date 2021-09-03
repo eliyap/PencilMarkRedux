7a67fad031f4ef157c3bf29580c4001f8751a423
Ipsky <yap@usc.edu>
2021-09-03 09:40:46 -0700

    draft range finder

--- PMAST/Sources/PMAST/Types/Literal/Code.swift
+++ PMAST/Sources/PMAST/Types/Literal/Code.swift
@@ -18,6 +18,8 @@ public final class Code: Literal {
     /// Not sure what this is.
     var meta: String?
     
+    var codeRange: NSRange! = nil
+    
     required init?(dict: [AnyHashable : Any]?, parent: Parent?, text: String) {
         let lang = dict?["lang"] as? String
         let meta = dict?["meta"] as? String
@@ -25,6 +27,29 @@ public final class Code: Literal {
         self.lang = lang
         self.meta = meta
         super.init(dict: dict, parent: parent, text: text)
+        
+        let contents: String = String(text[position.nsRange])
+        let nsContents = NSString(string: contents)
+        
+        if value.isEmpty {
+            /// Must be a fenced (not indented) block!
+            precondition(contents[0..<3] == "~~~" || contents[0..<3] == "```")
+            if contents.contains(where: \.isNewline) {
+                let newlineRange: NSRange = nsContents.rangeOfCharacter(from: .newlines)
+                precondition(newlineRange.lowerBound != NSNotFound)
+                codeRange = NSMakeRange(newlineRange.upperBound, 0)
+            } else {
+                /// Filter out corner case `~~~~~~` (3 open, 3 close)
+                codeRange = NSMakeRange(3, 0)
+            }
+        } else {
+            codeRange = nsContents.range(of: value)
+            precondition(codeRange.lowerBound != NSNotFound, """
+                Could not find value in fence!
+                V: '\(value)'
+                C: '\(contents)'
+                """)
+        }
     }
     
     override func style(_ string: NSMutableAttributedString) {
