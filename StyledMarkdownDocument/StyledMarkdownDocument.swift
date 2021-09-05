f00338d238de3f15882d0af619b03b58b036799a
Ipsky <yap@usc.edu>
2021-09-05 14:42:03 -0700

    break out protocol

--- StyledMarkdownDocument/StyledMarkdownDocument.swift
+++ StyledMarkdownDocument/StyledMarkdownDocument.swift
@@ -9,18 +9,6 @@ import UIKit
 import Combine
 import PMAST
 
-protocol MockableDocument {
-    var documentState: UIDocument.State { get }
-    var fileURL: URL { get }
-    var markdown: Markdown { get set }
-    var undoManager: UndoManager! { get }
-    var localizedName: String { get }
-    func close(completionHandler: ((Bool) -> Void)?)
-    func updateChangeCount(_ change: UIDocument.ChangeKind)
-    func save(to url: URL, for saveOperation: UIDocument.SaveOperation, completionHandler: ((Bool) -> Void)?)
-    func open(completionHandler: ((Bool) -> Void)?)
-}
-
 final class StyledMarkdownDocument: UIDocument, MockableDocument {
     
     /// Custom Markdown Model Object
