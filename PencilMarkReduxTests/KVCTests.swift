2332787255c37c03289bbb6af1c69357444154b6
Ipsky <yap@usc.edu>
2021-09-05 15:30:18 -0700

    extend and fix test

--- PencilMarkReduxTests/KVCTests.swift
+++ PencilMarkReduxTests/KVCTests.swift
@@ -19,23 +19,36 @@ class KVCTests: XCTestCase {
     
     }
 
-    func testBasic() throws {
-        /// Erase everything.
+    func testBasicEraseAll() throws {
         let text = "SOME TEXT"
         let document = MockDocument(text)
         let model = DrawableMarkdownViewController.Model(document: document, onSetTool: { /* nothing */})
         let kvc = KeyboardViewController(model: model)
-        kvc.__erase(NSMakeRange(0, 9))
+        kvc.present(topInset: .zero) /// must be called to activate `textView`!
         
         /// Check state.
+        XCTAssert(document.markdown.plain == text)
+        XCTAssert(kvc.textView.text == text)
+        checkASTConsistent(document.markdown.ast, with: text)
+        
+        /// Erase everything.
+        kvc.__erase(NSMakeRange(0, 9))
         let newValue = ""
         XCTAssert(document.markdown.plain == newValue)
+        XCTAssert(kvc.textView.text == newValue)
         checkASTConsistent(document.markdown.ast, with: newValue)
         
         /// Undo it.
         model.cmdC.undo.send()
         XCTAssertEqual(document.markdown.plain, text)
+        XCTAssert(kvc.textView.text == text)
         checkASTConsistent(document.markdown.ast, with: text)
+        
+        /// Redo it.
+        model.cmdC.redo.send()
+        XCTAssertEqual(document.markdown.plain, newValue)
+        XCTAssert(kvc.textView.text == newValue)
+        checkASTConsistent(document.markdown.ast, with: newValue)
     }
     
     func checkASTConsistent(_ ast: Root, with string: String) {
