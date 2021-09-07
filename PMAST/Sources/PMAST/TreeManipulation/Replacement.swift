54183f9b53bd93f696b79c907a470a8d417717f1
Ipsky <yap@usc.edu>
2021-09-07 09:29:45 -0700

    add sort and keypath var to Replacement

--- PMAST/Sources/PMAST/TreeManipulation/Replacement.swift
+++ PMAST/Sources/PMAST/TreeManipulation/Replacement.swift
@@ -13,3 +13,41 @@ struct Replacement {
     let replacement: String
 }
 
+extension Replacement {
+    /// Indicates that this replacement removes or inserts something.
+    var isNotNoOp: Bool {
+        return range.length != 0 || replacement.isEmpty == false
+    }
+}
+
+extension Replacement: Comparable {
+    /**
+     Sort in descending order of position.
+     Sort insertions to be after deletions (an insertion has ``range`` length 0).
+     
+     Replacement should go:
+     1. remove: (X, 2) => ""
+     2. insert: (X, 0) => "~~"
+     
+     Then combined they are:
+     - replace: (X, 2) => "~~"
+     */
+    static func < (lhs: Replacement, rhs: Replacement) -> Bool {
+        /// First, sort by descending order of position.
+        guard lhs.range.lowerBound == rhs.range.lowerBound else {
+            return lhs.range.lowerBound > rhs.range.lowerBound
+        }
+        
+        switch (lhs.range.length, rhs.range.length) {
+        case (0, 0):
+            assert(false, "Ambiguous Ordering of Insertion!\nLHS: \(lhs)\nRHS: \(rhs)")
+            return true
+        case (_, 0):
+            return true
+        case (0, _):
+            return false
+        default:
+            fatalError("Overlapping Ranges!")
+        }
+    }
+}
