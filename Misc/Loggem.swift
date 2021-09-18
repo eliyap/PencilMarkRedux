fa4077cd18de5ee2c7ff88f209a2444f12c9aa8d
Ipsky <yap@usc.edu>
2021-09-18 08:34:44 -0700

    Update Loggem.swift

--- Misc/Loggem.swift
+++ Misc/Loggem.swift
@@ -6,28 +6,30 @@
 //
 
 import Foundation
+import OSLog
 
-protocol Logger {
-    /// Subsystem name.
-    static var name: String { get }
+/// Docs: https://developer.apple.com/documentation/os/logging/generating_log_messages_from_your_code
+internal class PMLogger {
     
-    /// Whether this logger is enabled.
-    static var enabled: Bool { get }
+    static let AppSystem = "md.pencil"
     
-    /// Print function.
-    static func log<S: CustomStringConvertible>(_ s: S) -> Void
-}
-
-/// Default implementation
-extension Logger {
-    static func log<S: CustomStringConvertible>(_ s: S) -> Void {
-        if enabled {
-            print(name, s)
-        }
+    let subsystem: String
+    let enabled: Bool
+    let logger: Logger
+    
+    init(name: String, enabled: Bool) {
+        self.subsystem = Self.AppSystem + name
+        self.enabled = enabled
+        self.logger = Logger(subsystem: subsystem, category: "")
+    }
+    
+    func log<S: CustomStringConvertible>(_ s: S) -> Void {
+        logger.log(level: .info, "\(s)")
+    }
+    
+    func print<S: CustomStringConvertible>(_ s: S) -> Void {
+        logger.log(level: .debug, "\(s)")
     }
 }
 
-enum SceneRestoration: Logger {
-    static var name: String { "SceneRestoration" }
-    static var enabled: Bool { true }
-}
+let SceneRestoration = PMLogger(name: "SceneRestoration", enabled: true)
