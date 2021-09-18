181d78eaffba2d9a3eff3a86c90b720406c0b012
Ipsky <yap@usc.edu>
2021-09-18 11:14:14 -0700

    apply color to link

--- PMAST/Sources/PMAST/Types/Parent/Phrasing/Link.swift
+++ PMAST/Sources/PMAST/Types/Parent/Phrasing/Link.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import UIKit
 
 public final class Link: Parent {
     override class var type: String { "link" }
@@ -31,7 +32,48 @@ public final class Link: Parent {
     
     override func style(_ string: NSMutableAttributedString) {
         super.style(string)
-        print(string.string[position.nsRange])
         
+        /// Color in URL.
+        let linkText = string.string[position.nsRange]
+        var urlRange: NSRange = linkText.nsRanges(of: url).last!
+        urlRange = urlRange.offset(by: position.nsRange.lowerBound)
+        string.addAttribute(.foregroundColor, value: UIColor.blue, range: urlRange)
+    }
+}
+
+extension StringProtocol {
+    /// Find all ranges of a particular substring.
+    func ranges(of substr: String) -> [Range<Index>] {
+        var result: [Range<Index>] = []
+        var idx = startIndex
+        while idx < index(endIndex, offsetBy: -substr.count) {
+            if let range = self[idx..<index(idx, offsetBy: substr.count)].range(of: substr) {
+                result.append(range)
+            }
+            idx = index(after: idx)
+        }
+        return result
+    }
+    
+    /// Find all `NSRange`s of a particular substring.
+    func nsRanges(of substr: String) -> [NSRange] {
+        var result: [NSRange] = []
+        var idx = startIndex
+        while idx < index(endIndex, offsetBy: -substr.count) {
+            if let range = self[idx..<index(idx, offsetBy: substr.count)].range(of: substr) {
+                let low = range.lowerBound.utf16Offset(in: self)
+                let upp = range.upperBound.utf16Offset(in: self)
+                print(low, upp)
+                result.append(NSMakeRange(low, upp - low))
+            }
+            idx = index(after: idx)
+        }
+        return result
+    }
+}
+
+fileprivate extension NSRange {
+    func offset(by offset: Int) -> NSRange {
+        NSMakeRange(offset + lowerBound, length)
     }
 }
