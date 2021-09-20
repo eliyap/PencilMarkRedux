01dba8d5db5e6a28d7f32e7b82f57a23c17cf2d1
Ipsky <yap@usc.edu>
2021-09-20 08:44:17 -0700

    implement experimental custom cell

--- FileBrowser/FilesView.swift
+++ FileBrowser/FilesView.swift
@@ -8,8 +8,8 @@
 import UIKit
 
 final class FilesView: UITableView {
-    override func dequeueReusableCell(withIdentifier identifier: String) -> UITableViewCell? {
-        super.dequeueReusableCell(withIdentifier: identifier) ?? UITableViewCell()
+    override func dequeueReusableCell(withIdentifier identifier: String) -> FilesViewCell? {
+        super.dequeueReusableCell(withIdentifier: identifier) as? FilesViewCell
     }
     
     /// Scroll to the passed index path, which is assumed to be valid.
@@ -25,5 +25,19 @@ final class FilesView: UITableView {
             }
             self.scrollToRow(at: indexPath, at: .none, animated: true)
         }
+    }
+}
+
+final class FilesViewCell: UITableViewCell {
+    
+    static let identifier: String = "FilesViewCell"
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        textLabel?.font = UIFont.monospacedSystemFont(ofSize: 100, weight: .black)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
--- FileBrowser/FilesViewController.swift
+++ FileBrowser/FilesViewController.swift
@@ -39,6 +39,8 @@ class FilesViewController: UITableViewController {
         /// Attach custom `UITableView`
         self.tableView = filesView
         
+        tableView.register(FilesViewCell.self, forCellReuseIdentifier: FilesViewCell.identifier)
+        
         /// Attach refresh controller
         configureRefresh()
         
