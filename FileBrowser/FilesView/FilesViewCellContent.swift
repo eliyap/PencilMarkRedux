4e9f26ce1996296faf8231b597f540828c9bab3b
Ipsky <yap@usc.edu>
2021-09-20 19:04:46 -0700

    draft new appearance for cell

--- FileBrowser/FilesView/FilesViewCellContent.swift
+++ FileBrowser/FilesView/FilesViewCellContent.swift
@@ -18,19 +18,39 @@ extension FilesViewCell {
             /// Nothing...
             self
         }
-        
-        
     }
     
     final class ContentView: UIView, UIContentView {
         var configuration: UIContentConfiguration
         
+        var imageView = UIImageView(image: UIImage(named: "pencil.square"))
+        var labelView = UILabel()
+        
         init(configuration: UIContentConfiguration) {
             self.configuration = configuration
             super.init(frame: .zero)
+                        
+            labelView.text = "HELLO"
+            let stackView = UIStackView(arrangedSubviews: [
+                imageView,
+                labelView,
+                UIView() /// spacer view
+            ])
+            stackView.translatesAutoresizingMaskIntoConstraints = false
+            stackView.axis = .horizontal
+            stackView.alignment = .center
+            addSubview(stackView)
+            
+            let constraints = [
+                stackView.widthAnchor.constraint(equalTo: widthAnchor),
+                stackView.heightAnchor.constraint(equalTo: heightAnchor),
+                stackView.topAnchor.constraint(equalTo: topAnchor),
+                stackView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            ]
+            NSLayoutConstraint.activate(constraints)
         }
         
-        required init?(coder: NSCoder) {
+        required init(coder: NSCoder) {
             fatalError("init(coder:) has not been implemented")
         }
     }
